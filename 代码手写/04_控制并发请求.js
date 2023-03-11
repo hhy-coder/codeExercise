154ceddd6c764d9f75c2044f725aabcf62228364
hhy <2258545101@qq.com>
2023-03-11 13:41:16 +0800

    feat:闭包与柯里化

--- 代码手写/04_控制并发请求.js
+++ 代码手写/04_控制并发请求.js
@@ -35,36 +35,55 @@ function withConcurrency(request, { max = 5 } = {}) {
   return newReqest;
 }
 
-// const [text, setText] = useState('Initial value');
-// const textRef = useRef(text);
-// const handleClick= useCallback(() => {
-//      console.log(textRef.current);
-//  }, []); 
 
-//  useEffect(() => {
-//      console.log('update text')
-//      textRef.current = text;
-//  }, [text])
+// 并发请求函数
+const concurrencyRequest = (urls, maxNum) => {
+  return new Promise((resolve) => {
+      if (urls.length === 0) {
+          resolve([]);
+          return;
+      }
+      const results = [];
+      let index = 0; // 下一个请求的下标
+      let count = 0; // 当前请求完成的数量
 
-function reducer(state, action) {
-  switch(action.type) {
-      case 'update':
-          return action.preload;
-      case 'childComponent':
-          // 要执行的函数  
-          return state;     
-  }
+      // 发送请求
+      async function request() {
+          if (index === urls.length) return;
+          const i = index; // 保存序号，使result和urls相对应
+          const url = urls[index];
+          index++;
+          console.log(url);
+          try {
+              const resp = await fetch(url);
+              // resp 加入到results
+              results[i] = resp;
+          } catch (err) {
+              // err 加入到results
+              results[i] = err;
+          } finally {
+              count++;
+              // 判断是否所有的请求都已完成
+              if (count === urls.length) {
+                  console.log('完成了');
+                  resolve(results);
+              }
+              request();
+          }
+      }
+
+      // maxNum和urls.length取最小进行调用
+      const times = Math.min(maxNum, urls.length);
+      for(let i = 0; i < times; i++) {
+          request();
+      }
+  })
 }
-export default function Index() { // 父组件
-  const [state, dispatch] = useReducer(reducer, 'Initial value');
-
-  return (
-      <>
-          <input value={state} onChange={(e) => dispatch({
-              type: 'update', 
-              preload: e.target.value
-          })} />
-          <ChildComponent dispatch={dispatch} />
-      </>
-  )
+// 测试
+const urls = [];
+for (let i = 1; i <= 20; i++) {
+    urls.push(`https://jsonplaceholder.typicode.com/todos/${i}`);
 }
+concurrencyRequest(urls, 3).then(res => {
+    console.log(res);
+})
